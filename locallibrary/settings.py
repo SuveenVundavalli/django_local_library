80fe9f38bfe7afe55542df6e6e2acb48c1709ae5
Suveen Kumar Vundavalli <suveenkumar.vundavalli@gmail.com>
2018-01-18 19:40:54 +0530

    Changed psycopg2 version to 2.7.3.2 and allow hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -148,3 +148,9 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
